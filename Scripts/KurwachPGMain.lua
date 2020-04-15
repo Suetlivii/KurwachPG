71a1381906cfde8b72b7aca7c59e0a1b755a8b58
хофкс <hawxbf@gmail.com>
2020-04-16 00:24:18 +0300

    Переименовал пункт радиоменю в "Show Mission"

--- Scripts/KurwachPGMain.lua
+++ Scripts/KurwachPGMain.lua
@@ -3,10 +3,10 @@ Debug = Debuger:New(ME_isDebugMode)
 --Adding menu commands ----------------------------------------------------------------------------------
 --For fixed wing (FW) aircraft
 FixedWingGroupMenuManager = GroupMenuManager:New()
-FixedWingGroupMenuManager:AddMenuCommandForGroupsWithPrefix("FW", nil, "Show current task")
+FixedWingGroupMenuManager:AddMenuCommandForGroupsWithPrefix("FW", nil, "Show mission")
 --For rotary wing (RW) aircraft
 RotaryWingGroupMenuManager = GroupMenuManager:New()
-RotaryWingGroupMenuManager:AddMenuCommandForGroupsWithPrefix("RW", nil, "Show current task")
+RotaryWingGroupMenuManager:AddMenuCommandForGroupsWithPrefix("RW", nil, "Show mission")
 
 
 --Adding task managers -----------------------------------------------------------------------------------
@@ -33,16 +33,16 @@ RedRotaryWingTaskManager:SetFirstTask(ME_first_rr)
 RedRotaryWingTaskManager:StartTasking()
 
 
---Adding task report managers that will show task brief on "Show current task" menu command for FIXED wing aircraft
-BlueFixedWingBriefTaskReportManager = TaskReportManager:New(BlueFixedWingTaskManager, "Show current task", 45)
-RedFixedWingBriefTaskReportManager = TaskReportManager:New(RedFixedWingTaskManager, "Show current task", 45)
+--Adding task report managers that will show task brief on "Show mission" menu command for FIXED wing aircraft
+BlueFixedWingBriefTaskReportManager = TaskReportManager:New(BlueFixedWingTaskManager, "Show mission", 45)
+RedFixedWingBriefTaskReportManager = TaskReportManager:New(RedFixedWingTaskManager, "Show mission", 45)
 --Adding those task report managers to menu commnad managers
 FixedWingGroupMenuManager:AddListener(BlueFixedWingBriefTaskReportManager)
 FixedWingGroupMenuManager:AddListener(RedFixedWingBriefTaskReportManager)
 
---Adding task report managers that will show task brief on "Show current task" menu command for ROTARY wing aircraft
-BlueRotaryWingBriefTaskReportManager = TaskReportManager:New(BlueRotaryWingTaskManager, "Show current task", 45)
-RedRotaryWingBriefTaskReportManager = TaskReportManager:New(RedRotaryWingTaskManager, "Show current task", 45)
+--Adding task report managers that will show task brief on "Show mission" menu command for ROTARY wing aircraft
+BlueRotaryWingBriefTaskReportManager = TaskReportManager:New(BlueRotaryWingTaskManager, "Show mission", 45)
+RedRotaryWingBriefTaskReportManager = TaskReportManager:New(RedRotaryWingTaskManager, "Show mission", 45)
 --Adding those task report managers to menu commnad managers
 RotaryWingGroupMenuManager:AddListener(BlueRotaryWingBriefTaskReportManager)
 RotaryWingGroupMenuManager:AddListener(RedRotaryWingBriefTaskReportManager)
