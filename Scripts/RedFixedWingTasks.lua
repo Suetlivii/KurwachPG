f855c974ec1e77227bbd123a4cab381d8790fdd7
хофкс <hawxbf@gmail.com>
2020-04-16 00:03:18 +0300

    Добавил триггеры и скрипты для мисок красных самолетных мисок 1-9 Добавил самолетам игроков префикс FW для работы скриптов

--- Scripts/RedFixedWingTasks.lua
+++ Scripts/RedFixedWingTasks.lua
@@ -9,14 +9,10 @@ red01_task =
     goodEndTrigger = "10102",
     badEndTrigeer = "10103",
     cancelTrigger = "10104",
-    startMsgFriendly = [["Тестовая задача 1"
-
-Брифинг тестовой задачи 1
-
-Конец брифинга.
-    ]],
+    startMsgFriendly = [["Старая Классика"
+Турецкие скоростные лодки захватили танкер в квадрате DQ57. Уничтожить их!]],
     startMsgEnemy = "",
-    goodEndMsgFriendly = 'Задача 1 выполнена',
+    goodEndMsgFriendly = 'Задача "Старая Классика" выполнена!',
     goodEndMsgEnemy = "",
     badEndMsgFriendly = "",
     badEndMsgEnemy = "",
@@ -24,8 +20,8 @@ red01_task =
     cancelEndMsgEnemy = "",
     briefMsgFriendly = nil,
     briefMsgEnemy = "",
-    markZoneName = "r01zone",
-    markText = 'Текст маркера задачи 1'
+    markZoneName = "",
+    markText = ''
 }
 -------------------------------------------------------------------------------------------------------
 red02_task = 
@@ -36,14 +32,33 @@ red02_task =
     goodEndTrigger = "10202",
     badEndTrigeer = "10203",
     cancelTrigger = "10204",
-    startMsgFriendly = [["Тестовая задача 2"
-
-Брифинг тестовой задачи 2
-
-Конец брифинга.
-    ]],
+    startMsgFriendly = [["Remove Kebab"
+Разведка сообщает, что колонна техники ОАЭ движется из DP06 в район Аэродрома Khasab (DP-19), чтобы обеспечить плацдарм для наступления. Сделайте так, чтобы колонна не доехала.]],
     startMsgEnemy = "",
-    goodEndMsgFriendly = 'Задача 2 выполнена',
+    goodEndMsgFriendly = 'Задача "Remove Kebab" выполнена!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = 'Задача "Remove Kebab" провалена: колонна добралась до конечной точки.',
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red03_task = 
+{
+    name = 'r03',
+    coalition = 1,
+    startTrigger = "10301",
+    goodEndTrigger = "10302",
+    badEndTrigeer = "10303",
+    cancelTrigger = "10304",
+    startMsgFriendly = [["Островное приключение"
+Арабская Коалиция развернула комплекс ПВО на острове Tunb Kochak (CQ10), тем самым накрыв ракетным зонтиком немаленькую часть пролива. Вбомбите это дерьмо в каменный век!]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача "Островное приключение" выполнена!',
     goodEndMsgEnemy = "",
     badEndMsgFriendly = "",
     badEndMsgEnemy = "",
@@ -51,10 +66,155 @@ red02_task =
     cancelEndMsgEnemy = "",
     briefMsgFriendly = nil,
     briefMsgEnemy = "",
-    markZoneName = "r02zone",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red04_task = 
+{
+    name = 'r04',
+    coalition = 1,
+    startTrigger = "10401",
+    goodEndTrigger = "10402",
+    badEndTrigeer = "10403",
+    cancelTrigger = "10404",
+    startMsgFriendly = [["D-Fail"
+Арабская Коалиция высадила морской десант в квадрате CQ23. Разведка сообщила, что высадку прикрывает фрегат типа Perry.]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача "D-Fail" выполнена!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red05_task = 
+{
+    name = 'r05',
+    coalition = 1,
+    startTrigger = "10501",
+    goodEndTrigger = "10502",
+    badEndTrigeer = "10503",
+    cancelTrigger = "10504",
+    startMsgFriendly = [["Вмешательство"
+2 Фрегата типа Perry ВМС Пакистана входят в акваторию пролива (Квадрат DN). В случае агрессивного поведения - уничтожить!]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача "Вмешательство" выполнена!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red06_task = 
+{
+    name = 'r06',
+    coalition = 1,
+    startTrigger = "10601",
+    goodEndTrigger = "10602",
+    badEndTrigeer = "10603",
+    cancelTrigger = "10604",
+    startMsgFriendly = [["Морская Охота"
+Разведка сообщила - К северу от города Дубаи (Квадрат CP), стоит на рейде УРО класса "Тикондерога". Вы знаете что делать, Джентльмены!]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача "Морская Охота" выполнена!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red07_task = 
+{
+    name = 'r07',
+    coalition = 1,
+    startTrigger = "10701",
+    goodEndTrigger = "10702",
+    badEndTrigeer = "10703",
+    cancelTrigger = "10704",
+    startMsgFriendly = [["Портовые шлюхи"
+Разведка сообщает - В квадрате DP30 силы Арабской Коалиции готовят технику/людей для морского десанта. Нанесите "визит дружбы" в данный район.]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача "Портовые шлюхи" выполнена!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red08_task = 
+{
+    name = 'r08',
+    coalition = 1,
+    startTrigger = "10801",
+    goodEndTrigger = "10802",
+    badEndTrigeer = "10803",
+    cancelTrigger = "10804",
+    startMsgFriendly = [["Месть Сладка"
+РСЗО обстреливают район Аэродрома Khasab. Примерный Квадрат поиска Батареи РСЗО DP10]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача "Месть Сладка" выполнена!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = 'Задача "Месть Сладка" провалена: время на выполнение - 35 минут.',
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red09_task = 
+{
+    name = 'r09',
+    coalition = 1,
+    startTrigger = "10901",
+    goodEndTrigger = "10902",
+    badEndTrigeer = "10903",
+    cancelTrigger = "10904",
+    startMsgFriendly = [["Deep strike/penetration"/"Глубокое проникновение"
+Разведка сообщила, что в AL Ain International Airport (CM58) прибыла группа С-130 с очень важными грузами. Вы должны их уничтожить. Будьте внимательны! Разведка доложила, что аэропорт прикрывает 1 батарея ЗРК "Пэтриот". Для вашей поддержки в районе EN будет курсировать Заправщик Ил-78М]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача "Глубокое проникновение" выполнена!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
     markText = ''
 }
 -------------------------------------------------------------------------------------------------------
 
 table.insert(redFixedWingTasksConfig, red01_task)
 table.insert(redFixedWingTasksConfig, red02_task)
+table.insert(redFixedWingTasksConfig, red03_task)
+table.insert(redFixedWingTasksConfig, red04_task)
+table.insert(redFixedWingTasksConfig, red05_task)
+table.insert(redFixedWingTasksConfig, red06_task)
+table.insert(redFixedWingTasksConfig, red07_task)
+table.insert(redFixedWingTasksConfig, red08_task)
+table.insert(redFixedWingTasksConfig, red09_task)
