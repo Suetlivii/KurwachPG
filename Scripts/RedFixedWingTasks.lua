a6e374e0a2cf2a5b401ce5fdf3ced9687c7ef934
CptAnony <xbcemrus@googlemail.com>
2020-04-16 18:00:59 +0300

    Перевел брифинг на Английский (Потребуется редакция в будущем) Убрал тестовые изменения

--- Scripts/RedFixedWingTasks.lua
+++ Scripts/RedFixedWingTasks.lua
@@ -11,7 +11,10 @@ red01_task =
     badEndTrigeer = "10103",
     cancelTrigger = "10104",
     startMsgFriendly = [["Старая Классика"
-Турецкие скоростные лодки захватили танкер в квадрате DQ57. Уничтожить их!]],
+Турецкие скоростные лодки захватили танкер в квадрате DQ57. Уничтожить их!
+
+"Old Classics"
+Turkish speedboats captured a tanker in DQ57 square. Destroy them!]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача "Старая Классика" выполнена!',
     goodEndMsgEnemy = "",
@@ -34,7 +37,10 @@ red02_task =
     badEndTrigeer = "10203",
     cancelTrigger = "10204",
     startMsgFriendly = [["Remove Kebab"
-Разведка сообщает, что колонна техники ОАЭ движется из DP06 в район Аэродрома Khasab (DP-19), чтобы обеспечить плацдарм для наступления. Сделайте так, чтобы колонна не доехала.]],
+Разведка сообщает, что колонна техники ОАЭ движется из DP06 в район Аэродрома Khasab (DP-19), чтобы обеспечить плацдарм для наступления. Сделайте так, чтобы колонна не доехала.
+
+"Remove Kebab"
+Intelligence reports -The UAE convoy is moving from DP06 to the Khasab Airfield area (DP-19) to provide a bridgehead for the offensive. Make sure that the column does not reach.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача "Remove Kebab" выполнена!',
     goodEndMsgEnemy = "",
@@ -57,7 +63,10 @@ red03_task =
     badEndTrigeer = "10303",
     cancelTrigger = "10304",
     startMsgFriendly = [["Островное приключение"
-Арабская Коалиция развернула комплекс ПВО на острове Tunb Kochak (CQ10), тем самым накрыв ракетным зонтиком немаленькую часть пролива. Вбомбите это дерьмо в каменный век!]],
+Арабская Коалиция развернула комплекс ПВО на острове Tunb Kochak (CQ10), тем самым накрыв ракетным зонтиком немаленькую часть пролива. Вбомбите это дерьмо в каменный век!
+
+"Island adventure"
+The Arab Coalition has deployed a SAM unit on the island of Tunb Kochak (CQ10), thereby covering a large part of the strait with a anti-air umbrella. Bomb this shit in the stone age!]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача "Островное приключение" выполнена!',
     goodEndMsgEnemy = "",
@@ -80,7 +89,10 @@ red04_task =
     badEndTrigeer = "10403",
     cancelTrigger = "10404",
     startMsgFriendly = [["D-Fail"
-Арабская Коалиция высадила морской десант в квадрате CQ23. Разведка сообщила, что высадку прикрывает фрегат типа Perry.]],
+Арабская Коалиция высадила морской десант в квадрате CQ23. Разведка сообщила, что высадку прикрывает фрегат типа Perry.
+
+"D-Fail"
+The Arab Coalition has landed in CQ23. Intelligence reported that the landing zone was covered by a Perry Class frigate.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача "D-Fail" выполнена!',
     goodEndMsgEnemy = "",
@@ -103,7 +115,10 @@ red05_task =
     badEndTrigeer = "10503",
     cancelTrigger = "10504",
     startMsgFriendly = [["Вмешательство"
-2 Фрегата типа Perry ВМС Пакистана входят в акваторию пролива (Квадрат DN). В случае агрессивного поведения - уничтожить!]],
+2 Фрегата типа Perry ВМС Пакистана входят в акваторию пролива (Квадрат DN). В случае агрессивного поведения - уничтожить!
+
+"Intervention"
+2 Perry class frigates of the Pakistan Navy enter the strait water area (DN Square). In case of aggressive behavior - destroy them!]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача "Вмешательство" выполнена!',
     goodEndMsgEnemy = "",
@@ -126,7 +141,10 @@ red06_task =
     badEndTrigeer = "10603",
     cancelTrigger = "10604",
     startMsgFriendly = [["Морская Охота"
-Разведка сообщила - К северу от города Дубаи (Квадрат CP), стоит на рейде УРО класса "Тикондерога". Вы знаете что делать, Джентльмены!]],
+Разведка сообщила - К северу от города Дубаи (Квадрат CP), стоит на рейде Крейсер УРО класса "Тикондерога". Вы знаете что делать, Джентльмены!
+
+"Sea Hunt"
+Intelligence reported - At north of the Dubai-city (CP Square), is Ticonderoga class CG. You know what to do, gentlemen!]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача "Морская Охота" выполнена!',
     goodEndMsgEnemy = "",
@@ -149,7 +167,10 @@ red07_task =
     badEndTrigeer = "10703",
     cancelTrigger = "10704",
     startMsgFriendly = [["Портовые шлюхи"
-Разведка сообщает - В квадрате DP30 силы Арабской Коалиции готовят технику/людей для морского десанта. Нанесите "визит дружбы" в данный район.]],
+Разведка сообщает - В квадрате DP30 силы Арабской Коалиции готовят технику/людей для морского десанта. Нанесите "визит дружбы" в данный район.
+
+"Port whores"
+Intelligence reports - In the DP30 square, the forces of the Arab Coalition are preparing equipment and people for amphibious assault. Make a "friendship visit" to the area.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача "Портовые шлюхи" выполнена!',
     goodEndMsgEnemy = "",
@@ -172,7 +193,10 @@ red08_task =
     badEndTrigeer = "10803",
     cancelTrigger = "10804",
     startMsgFriendly = [["Месть Сладка"
-РСЗО обстреливают район Аэродрома Khasab. Примерный Квадрат поиска Батареи РСЗО DP10]],
+РСЗО обстреливают район Аэродрома Khasab. Примерный Квадрат поиска Батареи РСЗО DP10
+
+"Sweet revenge"
+MLRS shell area of the Khasab Airfield. Approximate Square of search Battery MLRS DP10]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача "Месть Сладка" выполнена!',
     goodEndMsgEnemy = "",
@@ -194,31 +218,11 @@ red09_task =
     goodEndTrigger = "10902",
     badEndTrigeer = "10903",
     cancelTrigger = "10904",
-    startMsgFriendly = [["Deep strike/penetration"/"Глубокое проникновение"
-Разведка сообщила, что в AL Ain International Airport (CM58) прибыла группа С-130 с очень важными грузами. Вы должны их уничтожить. Будьте внимательны! Разведка доложила, что аэропорт прикрывает 1 батарея ЗРК "Пэтриот". Для вашей поддержки в районе EN будет курсировать Заправщик Ил-78М]],
-    startMsgEnemy = "",
-    goodEndMsgFriendly = 'Задача "Глубокое проникновение" выполнена!',
-    goodEndMsgEnemy = "",
-    badEndMsgFriendly = "",
-    badEndMsgEnemy = "",
-    cancelEndMsgFriendly = "",
-    cancelEndMsgEnemy = "",
-    briefMsgFriendly = nil,
-    briefMsgEnemy = "",
-    markZoneName = "",
-    markText = ''
-}
--------------------------------------------------------------------------------------------------------
-red09_task = 
-{
-    name = 'r09',
-    coalition = 1,
-    startTrigger = "10901",
-    goodEndTrigger = "10902",
-    badEndTrigeer = "10903",
-    cancelTrigger = "10904",
-    startMsgFriendly = [["Deep strike/penetration"/"Глубокое проникновение"
-Разведка сообщила, что в AL Ain International Airport (CM58) прибыла группа С-130 с очень важными грузами. Вы должны их уничтожить. Будьте внимательны! Разведка доложила, что аэропорт прикрывает 1 батарея ЗРК "Пэтриот". Для вашей поддержки в районе EN будет курсировать Заправщик Ил-78М]],
+    startMsgFriendly = [["Глубокое проникновение"
+Разведка сообщила, что в AL Ain International Airport (CM58) прибыла группа С-130 с очень важными грузами. Вы должны их уничтожить. Будьте внимательны! Наши агенты докладывают, что аэропорт прикрывает 1 батарея ЗРК "Пэтриот". Для вашей поддержки в районе EN будет курсировать Заправщик Ил-78М.
+
+"Deep penetration"
+Intelligence reported that a group of C-130s with very important cargo arrived at AL Ain International Airport (CM58). You must destroy them. Be careful! Our agent reported that the airport was covered by Patriot SAM. For your support, the IL-78M Tanker will run in the EN area]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача "Глубокое проникновение" выполнена!',
     goodEndMsgEnemy = "",
