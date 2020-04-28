5cada961a252b30e8228bc275d8032188cc73dbd
CptAnony <xbcemrus@googlemail.com>
2020-04-28 12:09:48 +0300

    Добавил 2 самолетные миссии. Больше у меня нету идей. Начинаю продумывать концепт вертолетных миссий. Скрипт: Добавил номера к брифингам миссий. Проверь 2 мисски (24,25) и делай игровую версию.

--- Scripts/RedFixedWingTasks.lua
+++ Scripts/RedFixedWingTasks.lua
@@ -10,10 +10,10 @@ red01_task =
     goodEndTrigger = "10102",
     badEndTrigeer = "10103",
     cancelTrigger = "10104",
-    startMsgFriendly = [["Старая Классика"
+    startMsgFriendly = [[01."Старая Классика"
 Турецкие скоростные лодки захватили танкер в квадрате DQ57. Уничтожить их!
 
-"Old Classics"
+01."Old Classics"
 Turkish speedboats captured a tanker in DQ57 square. Destroy them!]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -36,10 +36,10 @@ red02_task =
     goodEndTrigger = "10202",
     badEndTrigeer = "10203",
     cancelTrigger = "10204",
-    startMsgFriendly = [["Remove Kebab"
+    startMsgFriendly = [[02."Remove Kebab"
 Разведка сообщает, что колонна техники ОАЭ движется из DP06 в район Аэродрома Khasab (DP-19), чтобы обеспечить плацдарм для наступления. Сделайте так, чтобы колонна не доехала.
 
-"Remove Kebab"
+02."Remove Kebab"
 Intelligence reports -The UAE convoy is moving from DP06 to the Khasab Airfield area (DP-19) to provide a bridgehead for the offensive. Make sure that the column does not reach.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -62,10 +62,10 @@ red03_task =
     goodEndTrigger = "10302",
     badEndTrigeer = "10303",
     cancelTrigger = "10304",
-    startMsgFriendly = [["Островное приключение"
+    startMsgFriendly = [[03."Островное приключение"
 Арабская Коалиция развернула комплекс ПВО на острове Tunb Kochak (CQ10), тем самым накрыв ракетным зонтиком немаленькую часть пролива. Вбомбите это дерьмо в каменный век!
 
-"Island adventure"
+03."Island adventure"
 The Arab Coalition has deployed a SAM unit on the island of Tunb Kochak (CQ10), thereby covering a large part of the strait with a anti-air umbrella. Bomb this shit to the stone age!]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -88,10 +88,10 @@ red04_task =
     goodEndTrigger = "10402",
     badEndTrigeer = "10403",
     cancelTrigger = "10404",
-    startMsgFriendly = [["D-Fail"
+    startMsgFriendly = [[04."D-Fail"
 Арабская Коалиция высадила морской десант в квадрате CQ23. Разведка сообщила, что высадку прикрывает фрегат типа Perry.
 
-"D-Fail"
+04."D-Fail"
 The Arab Coalition has landed in CQ23. Intelligence reported that the landing zone was covered by a Perry Class frigate.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -114,10 +114,10 @@ red05_task =
     goodEndTrigger = "10502",
     badEndTrigeer = "10503",
     cancelTrigger = "10504",
-    startMsgFriendly = [["Вмешательство"
+    startMsgFriendly = [[05."Вмешательство"
 Фрегат типа Perry ВМС Пакистана входят в акваторию пролива (Квадрат DN). В случае агрессивного поведения - уничтожить!
 
-"Intervention"
+05."Intervention"
 Perry class frigate of the Pakistan Navy enter the strait water area (DN Square). In case of aggressive behavior - destroy them!]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -140,10 +140,10 @@ red06_task =
     goodEndTrigger = "10602",
     badEndTrigeer = "10603",
     cancelTrigger = "10604",
-    startMsgFriendly = [["Морская Охота"
+    startMsgFriendly = [[06."Морская Охота"
 Разведка сообщила - К северу от города Дубаи (Квадрат CP), стоит на рейде фрегат класса "Perry". Вы знаете что делать, Джентльмены!
 
-"Sea Hunt"
+06."Sea Hunt"
 Intelligence reported - North of the Dubai-city (CP Square), is "Perry" class CC. You know what to do, gentlemen!]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -166,10 +166,10 @@ red07_task =
     goodEndTrigger = "10702",
     badEndTrigeer = "10703",
     cancelTrigger = "10704",
-    startMsgFriendly = [["Портовые шлюхи"
+    startMsgFriendly = [[07."Портовые шлюхи"
 Разведка сообщает - В квадрате DP30 силы Арабской Коалиции готовят технику и людей для морского десанта. Нанесите "визит дружбы" в данный район.
 
-"Port whores"
+07."Port whores"
 Intelligence reports - In the DP30 square, the forces of the Arab Coalition are preparing equipment and people for amphibious assault. Make a "friendship visit" to the area.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -192,10 +192,10 @@ red08_task =
     goodEndTrigger = "10802",
     badEndTrigeer = "10803",
     cancelTrigger = "10804",
-    startMsgFriendly = [["Месть Сладка"
+    startMsgFriendly = [[08."Месть Сладка"
 РСЗО обстреливают район Аэродрома Khasab. Примерный Квадрат поиска Батареи РСЗО DP17
 
-"Sweet revenge"
+08."Sweet revenge"
 MLRS shell area of the Khasab Airfield. Approximate Square of search Battery MLRS DP17]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -218,10 +218,10 @@ red09_task =
     goodEndTrigger = "10902",
     badEndTrigeer = "10903",
     cancelTrigger = "10904",
-    startMsgFriendly = [["Глубокое проникновение"
+    startMsgFriendly = [[09."Глубокое проникновение"
 Разведка сообщила, что в AL Ain International Airport (CM58) прибыла группа С-130 с очень важными грузами. Вы должны их уничтожить. Будьте внимательны! Наши агенты докладывают, что аэропорт прикрывает 1 батарея ЗРК "Пэтриот". Для вашей поддержки в районе EN будет курсировать Заправщик Ил-78М.
 
-"Deep penetration"
+09."Deep penetration"
 Intelligence reported that a group of C-130s with very important cargo arrived at AL Ain International Airport (CM58). You must destroy them. Be careful! Our agent reported that the airport was covered by Patriot SAM's. For your support, the IL-78M Tanker will run in the EN area]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -244,10 +244,10 @@ red10_task =
     goodEndTrigger = "11002",
     badEndTrigeer = "11003",
     cancelTrigger = "11004",
-    startMsgFriendly = [["Флешбэки Тяныча"
+    startMsgFriendly = [[10."Флешбэки Тяныча"
 Патрульный вертолет обнаружил скопление противника в деревне Ash Shishan в квадрате DQ30. У противника не обнаружино ПВО. Разбомбите их!
 
-"Flashbacks of Tyanch"
+10."Flashbacks of Tyanch"
 A patrol helicopter detected enemy forces in Ash Shishan Village (DQ30). The enemy has no air defense. Bomb them!]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -270,10 +270,10 @@ red11_task =
     goodEndTrigger = "11102",
     badEndTrigeer = "11103",
     cancelTrigger = "11104",
-    startMsgFriendly = [["Безумие Сири"
+    startMsgFriendly = [[11."Безумие Сири"
 Вражеская коалиция собрала огромное количество iPhone 20 на острове Sirri Island (BP56). Оставим врагов без связи. Разбомбите 4 склада и уничтожьте всю технику противника на острове.
 
-"Siri's Madness"
+11."Siri's Madness"
 The enemy coalition has collected a huge number of iPhone 20 on Sirri Island (BP56). Leave the enemies without communication. Bomb 4 warehouses and destroy all enemy vehicles on the island.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -296,10 +296,10 @@ red12_task =
     goodEndTrigger = "11202",
     badEndTrigeer = "11203",
     cancelTrigger = "11204",
-    startMsgFriendly = [["Опасный копьеносец"
+    startMsgFriendly = [[12."Опасный копьеносец"
 2 B-1 Lancer направляются к нашей основной базе Bandar Abass из квадрата VL. Ваша задача перехватить их до точки сброса.
 
-"Dangerous lancer"
+12."Dangerous lancer"
 2 B-1 Lancer are heading to our main Bandar Abass base from the VL square. Your task is to intercept them before they reach the drop point.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -322,10 +322,10 @@ red13_task =
     goodEndTrigger = "11302",
     badEndTrigeer = "11303",
     cancelTrigger = "11304",
-    startMsgFriendly = [["Прихлопнуть мотылька"
+    startMsgFriendly = [[13."Прихлопнуть мотылька"
 Наблюдатели заметили 2 вертолета-разведчика у базы Khasab. Прихлопните эту мелочь!
 
-"slam the moth"
+13."slam the moth"
 Observers spotted 2 recon helicopters near Khasab base. Slam this little bitch!.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -348,10 +348,10 @@ red14_task =
     goodEndTrigger = "11402",
     badEndTrigeer = "11403",
     cancelTrigger = "11404",
-    startMsgFriendly = [["Пни малыша"
+    startMsgFriendly = [[14."Пни малыша"
 Самолет AWACS совершает патрулирование акватории пролива. Радары обнаружили, что он движется со стороны GM. Сбейте его!
 
-"Kick the baby"
+14."Kick the baby"
 Enemy AWACS patrols the strait. Radars have discovered that it is moving from the side of GM. Knock it down!.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -374,10 +374,10 @@ red15_task =
     goodEndTrigger = "11502",
     badEndTrigeer = "11503",
     cancelTrigger = "11504",
-    startMsgFriendly = [["Падение черного ястреба"
+    startMsgFriendly = [[15."Падение черного ястреба"
 Наш агент сообщил, что с базы Sharjah Intl (CP50) только что взлтели транспортные вертолеты с десантом! Цель высадки Tunb Island AFB! Перехватите их!
 
-"Black Hawk Down"
+15."Black Hawk Down"
 Our agent said that transport helicopters with an airborne assault just flew up from the Sharjah Intl base (CP50)! Landing target is Tunb Island AFB! Intercept them!.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -400,10 +400,10 @@ red16_task =
     goodEndTrigger = "11602",
     badEndTrigeer = "11603",
     cancelTrigger = "11604",
-    startMsgFriendly = [["Буйство Лебедя"
+    startMsgFriendly = [[16."Буйство Лебедя"
 Союзный Ту-160 взлетает с Jiroft Airport (ES67) для пуска крылатых ракет по вражеским целям. Доставьте его живым до точки сброса или эвакуации. Через 15 минут он пройдет над Bandar Abbas Intl. Встретьте его там!
 
-"BlackJack's Rampage"
+16."BlackJack's Rampage"
 The allied Tu-160 takes off from Jiroft Airport (ES67) to launch cruise missiles at enemy targets. Take him alive to the point of discharge or evacuation. After 15 minutes, he will pass over Bandar Abbas Intl. Meet the BlackJack there!.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -426,10 +426,10 @@ red17_task =
     goodEndTrigger = "11702",
     badEndTrigeer = "11703",
     cancelTrigger = "11704",
-    startMsgFriendly = [["Наказать кошечку"
+    startMsgFriendly = [[17."Наказать кошечку"
 С Kerman Airport (ES67) пришло срочное сообщение! Были уганы 2 F-14 иранских ВВС. Получено добро на их уничтожение!
 
-"Punish Kitty"
+17."Punish Kitty"
 An urgent message has arrived from Kerman Airport (ES67)! 2 F-14s of the Iranian Air Force were hijacked. Received the go-ahead for their destruction!.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -452,10 +452,10 @@ red18_task =
     goodEndTrigger = "11802",
     badEndTrigeer = "11803",
     cancelTrigger = "11804",
-    startMsgFriendly = [["Эрдоган уходи"
+    startMsgFriendly = [[18."Эрдоган уходи"
 Разведка сообщает: 2 тяжелых транспорта ВВС Турции - С-17 Globemaster летят в AL Ain International airport из квадрата TL. Перехватите их!
 
-"Piss off Erdorgan"
+18."Piss off Erdorgan"
 Intelligence reports: 2 Turkish Air Force C-17 Globemaster fly to AL Ain International airport from TL square. Intercept them!.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -478,10 +478,10 @@ red19_task =
     goodEndTrigger = "11902",
     badEndTrigeer = "11903",
     cancelTrigger = "11904",
-    startMsgFriendly = [["Укротитель торнадо"
+    startMsgFriendly = [[19."Укротитель торнадо"
 В квардрате WG замечены вражеские штурмовики идущие прямым курсом на базу Queshm Island. Укротите их!
 
-"Tornado Tamer"
+19."Tornado Tamer"
 Enemy attack aircraft were seen in WG square! They heading straight for Queshm Island Base. Tame them!.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -504,10 +504,10 @@ red20_task =
     goodEndTrigger = "12002",
     badEndTrigeer = "12003",
     cancelTrigger = "12004",
-    startMsgFriendly = [["Они на деревьях!"
+    startMsgFriendly = [[20."Они на деревьях!"
 Наши силы десантировались на остров Abu Musa (CP-06). Ваша задача непосредственная поддержака сил вторжения!
 
-"They are on the trees!"
+20."They are on the trees!"
 Our troops landed to Abu Musa island (CP-06). Your task is Close Air Support!.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -530,10 +530,10 @@ red21_task =
     goodEndTrigger = "12102",
     badEndTrigeer = "12103",
     cancelTrigger = "12104",
-    startMsgFriendly = [["Grand Theft Aero!"
+    startMsgFriendly = [[21."Grand Theft Aero!"
 Наш самолет ДРЛО сообщил, что его взяли в клещи в квадрате VM, и заставляют следовать в воздушное пространство врага. Ваша задача выручить наших товарищей! 
 "
-"Grand Theft Aero!"
+21."Grand Theft Aero!"
 Our AWACS reported an interception by two F-15s at grid VM and is forced to enter enemy airspace. Our task is to save our comrades!.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -556,10 +556,10 @@ red22_task =
     goodEndTrigger = "12202",
     badEndTrigeer = "12203",
     cancelTrigger = "12204",
-    startMsgFriendly = [["Нефтяное безумие!"
+    startMsgFriendly = [[22."Нефтяное безумие!"
 Что бы поднять цену на нефть, командование приказывает уничтожить 2 нефтяных платформы в квадрате CP76.
-"
-"Oil madness!"
+
+22."Oil madness!"
 To raise the price of oil, the command orders the destruction of 2 oil platforms in square CP76.]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
@@ -582,15 +582,67 @@ red23_task =
     goodEndTrigger = "12302",
     badEndTrigeer = "12303",
     cancelTrigger = "12304",
-    startMsgFriendly = [["Гонка по горам!"
+    startMsgFriendly = [[23."Гонка по горам!"
 Наши войска высадили некоторе количество техники на территорию противника. Они хотят занять выгодную позицию в горной деревне в квадрате DP13. Ваша задача оказать поддержку и прикрыть союзную колонну техники!
 
-"Mountain racing!"
+23."Mountain racing!"
 Our troops landed a certain amount of equipment on the territory of the enemy. They want to take a vantage point in a mountain village in square DP13. Your task is to provide support and cover the allied column of equipment!]],
     startMsgEnemy = "",
     goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
     goodEndMsgEnemy = "",
     badEndMsgFriendly = "Миссия провалена! Наша колонна разбита. / Mission failed! Our column have been destroyed",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red24_task = 
+{
+    name = 'r24',
+    coalition = 1,
+    startTrigger = "12401",
+    goodEndTrigger = "12402",
+    badEndTrigeer = "12403",
+    cancelTrigger = "12404",
+    startMsgFriendly = [[24."Гранд Отель"
+Радведка сообщает! Выскопоставленное лицо вражеской коалиции сделало опрометчивый шаг. И появился в Гранд Отеле на насыпном острове Palm Jumeirah (CN18). Ваша задача ликвидировать выскокопоставленное лицо и всю вражескую технику которая вам встретится.
+
+24."Grand Hotel"
+Radvedka reports! The face of the enemy coalition took a rash step. And appeared at the Grand Hotel on the bulk island of Palm Jumeirah (CN18). Your task is to eliminate the high-ranking person and all the enemy equipment that you will meet.]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "Миссия провалена! / Mission failed!",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red25_task = 
+{
+    name = 'r25',
+    coalition = 1,
+    startTrigger = "12501",
+    goodEndTrigger = "12502",
+    badEndTrigeer = "12503",
+    cancelTrigger = "12504",
+    startMsgFriendly = [[25."Плохая идея"
+Агент сообщает! К северо-западу от городка Al Ima (Северная чать DP46), стоит большой склад. В нем складируют оружие для диверсионных отрядов. Плохая идея держать все в одном здании...
+
+25."Bad Idea"
+Agent reports! To the northwest of the town of Al Ima (Northern Part DP46), there is a large warehouse. It stores weapons for sabotage units. It’s a bad idea to keep everything in one building ...]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "Миссия провалена! / Mission failed!",
     badEndMsgEnemy = "",
     cancelEndMsgFriendly = "",
     cancelEndMsgEnemy = "",
@@ -624,3 +676,5 @@ table.insert(redFixedWingTasksConfig, red20_task)
 table.insert(redFixedWingTasksConfig, red21_task)
 table.insert(redFixedWingTasksConfig, red22_task)
 table.insert(redFixedWingTasksConfig, red23_task)
+table.insert(redFixedWingTasksConfig, red24_task)
+table.insert(redFixedWingTasksConfig, red25_task)
