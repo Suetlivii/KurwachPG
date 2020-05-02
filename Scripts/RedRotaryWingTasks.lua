b49a421a6dbcfaaf83c5f0f96eee001161d699ca
CptAnony <xbcemrus@googlemail.com>
2020-05-02 10:43:18 +0300

    Добавлена версия 1.07.0 +4 верт миссии +3 верт группы на Khasab

--- Scripts/RedRotaryWingTasks.lua
+++ Scripts/RedRotaryWingTasks.lua
@@ -80,7 +80,115 @@ Enemies stole an alcohol tank from our airfield. Your task is to destroy the con
     markText = ''
 }
 -------------------------------------------------------------------------------------------------------
+red_heli_04_task = 
+{
+    name = 'rh04',
+    coalition = 1,
+    startTrigger = "30401",
+    goodEndTrigger = "30402",
+    badEndTrigeer = "30403",
+    cancelTrigger = "30404",
+    startMsgFriendly = [[04."Спасатели Малибу"
+Замечена группа противника в городке Ghassah (DQ30). 
+
+04."Baywatch Malibu"
+Enemy group spoted in Ghassah (DQ30).]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red_heli_05_task = 
+{
+    name = 'rh05',
+    coalition = 1,
+    startTrigger = "30501",
+    goodEndTrigger = "30502",
+    badEndTrigeer = "30503",
+    cancelTrigger = "30504",
+    startMsgFriendly = [[05."Сегодня будет град"
+Замечена группа РСЗО которая двигается в район развертывания (DP07). Уничтожте группу!
+
+05."Today will be hail"
+A group of MLRS that is moving to the deployment area (DP07) is seen. Destroy the group!]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "Миссия провалена! Группа противника достигла своей цели. / Mission failed! The enemy group has reached its target.",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red_heli_06_task = 
+{
+    name = 'rh06',
+    coalition = 1,
+    startTrigger = "30601",
+    goodEndTrigger = "30602",
+    badEndTrigeer = "30603",
+    cancelTrigger = "30604",
+    startMsgFriendly = [[06."Враг на пороге"
+Замечена группа пехоты всего в 3х километрах к западу от Khasab. Разбейте силы противника!
+
+06."Enemy on the verge"
+AA group of infantry was noticed just 3 kilometers west of Khasab. Break the enemy forces!]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
+red_heli_07_task = 
+{
+    name = 'rh07',
+    coalition = 1,
+    startTrigger = "30701",
+    goodEndTrigger = "30702",
+    badEndTrigeer = "30703",
+    cancelTrigger = "30704",
+    startMsgFriendly = [[07."Район №7"
+В городке Hayr Salam (DP38), местные жители сообщают о прибытии 3х БТР врага! Это слишком близко от нашей базы. Зачистите это место!
+
+07."District №7"
+In the town of Hayr Salam (DP38), local residents report the arrival of 3 enemy APC! It is too close to our base. Strip this place!]],
+    startMsgEnemy = "",
+    goodEndMsgFriendly = 'Задача выполнена! Mission complete!',
+    goodEndMsgEnemy = "",
+    badEndMsgFriendly = "",
+    badEndMsgEnemy = "",
+    cancelEndMsgFriendly = "",
+    cancelEndMsgEnemy = "",
+    briefMsgFriendly = nil,
+    briefMsgEnemy = "",
+    markZoneName = "",
+    markText = ''
+}
+-------------------------------------------------------------------------------------------------------
 
 table.insert(redRotaryWingTasksConfig, red_heli_01_task)
 table.insert(redRotaryWingTasksConfig, red_heli_02_task)
 table.insert(redRotaryWingTasksConfig, red_heli_03_task)
+table.insert(redRotaryWingTasksConfig, red_heli_04_task)
+table.insert(redRotaryWingTasksConfig, red_heli_05_task)
+table.insert(redRotaryWingTasksConfig, red_heli_06_task)
+table.insert(redRotaryWingTasksConfig, red_heli_07_task)
